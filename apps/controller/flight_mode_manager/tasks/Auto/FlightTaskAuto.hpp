4c6a4678989c73179350af0cfe2d570a619d82e9
yuliang <1403847@qq.com>
2024-07-16 09:25:38 +0800

    refactor(controller): improve code readability and maintainability in FlightTaskAuto
    - Enhance code readability by adjusting formatting and removing unnecessary comments.
    - Include guard macros have been updated to follow the correct naming convention.
    - Obstacle avoidance headers are now conditionally included based on flash constraints.
    - Update internal documentation comments for clarity and consistency.
    - The update focuses on cleaning up the codebase and improving maintainability without
      altering functionality.
    
    BREAKING CHANGE: Changes in formatting and code structure may affect downstream
    dependencies relying on the previous format.

--- apps/controller/flight_mode_manager/tasks/Auto/FlightTaskAuto.hpp
+++ apps/controller/flight_mode_manager/tasks/Auto/FlightTaskAuto.hpp
@@ -32,9 +32,9 @@
 
 // TODO: make this switchable in the board config, like a module
 #if CONSTRAINED_FLASH
-#include <avoidance/ObstacleAvoidance_dummy.hpp>
+#   include <avoidance/ObstacleAvoidance_dummy.hpp>
 #else
-#include <avoidance/ObstacleAvoidance.hpp>
+#   include <avoidance/ObstacleAvoidance.hpp>
 #endif
 
 /**
@@ -70,8 +70,11 @@ public:
     void overrideCruiseSpeed(const float cruise_speed_m_s) override;
 
 protected:
-    void _updateInternalWaypoints();                                          /**< Depending on state of vehicle, the internal waypoints might differ from target (for instance if offtrack). */
-    bool _compute_heading_from_2D_vector(float &heading, matrix::Vector2f v); /**< Computes and sets heading a 2D vector */
+    /**< Depending on state of vehicle, the internal waypoints might differ from target (for instance if offtrack). */
+    void _updateInternalWaypoints();
+
+    /**< Computes and sets heading a 2D vector */
+    bool _compute_heading_from_2D_vector(float &heading, matrix::Vector2f v);
 
     /** Reset position or velocity setpoints in case of EKF reset event */
     void _ekfResetHandlerPositionXY(const matrix::Vector2f &delta_xy) override;
@@ -93,15 +96,15 @@ protected:
     };
 
     void _prepareLandSetpoints();
-    bool _highEnoughForLandingGear(); /**< Checks if gears can be lowered. */
+    bool _highEnoughForLandingGear();           /**< Checks if gears can be lowered. */
 
-    void updateParams() override; /**< See ModuleParam class */
+    void updateParams() override;               /**< See ModuleParam class */
 
-    matrix::Vector3f _prev_prev_wp{}; /**< Pre-previous waypoint (local frame). This will be used for smoothing trajectories -> not used yet. */
-    matrix::Vector3f _prev_wp{};      /**< Previous waypoint  (local frame). If no previous triplet is available, the prev_wp is set to current position. */
+    matrix::Vector3f _prev_prev_wp{};           /**< Pre-previous waypoint (local frame). This will be used for smoothing trajectories -> not used yet. */
+    matrix::Vector3f _prev_wp{};                /**< Previous waypoint  (local frame). If no previous triplet is available, the prev_wp is set to current position. */
     bool             _prev_was_valid{false};
-    matrix::Vector3f _target{};  /**< Target waypoint  (local frame).*/
-    matrix::Vector3f _next_wp{}; /**< The next waypoint after target (local frame). If no next setpoint is available, next is set to target. */
+    matrix::Vector3f _target{};                 /**< Target waypoint  (local frame).*/
+    matrix::Vector3f _next_wp{};                /**< The next waypoint after target (local frame). If no next setpoint is available, next is set to target. */
     bool             _next_was_valid{false};
     float            _mc_cruise_speed{NAN};     /**< Requested cruise speed. If not valid, default cruise speed is used. */
     WaypointType     _type{WaypointType::idle}; /**< Type of current target triplet. */
@@ -131,13 +134,12 @@ protected:
 
     DEFINE_PARAMETERS_CUSTOM_PARENT(FlightTask,
                                     (ParamFloat<params_id::MPC_XY_CRUISE>)_param_mpc_xy_cruise,
-                                    (ParamFloat<params_id::NAV_MC_ALT_RAD>)
-                                        _param_nav_mc_alt_rad,                                // vertical acceptance radius at which waypoints are updated
-                                    (ParamInt<params_id::MPC_YAW_MODE>)_param_mpc_yaw_mode,   // defines how heading is executed,
-                                    (ParamInt<params_id::COM_OBS_AVOID>)_param_com_obs_avoid, // obstacle avoidance active
+                                    (ParamFloat<params_id::NAV_MC_ALT_RAD>)_param_nav_mc_alt_rad, // vertical acceptance radius at which waypoints are updated
+                                    (ParamInt<params_id::MPC_YAW_MODE>)_param_mpc_yaw_mode,       // defines how heading is executed,
+                                    (ParamInt<params_id::COM_OBS_AVOID>)_param_com_obs_avoid,     // obstacle avoidance active
                                     (ParamFloat<params_id::MPC_YAWRAUTO_MAX>)_param_mpc_yawrauto_max,
-                                    (ParamFloat<params_id::MIS_YAW_ERR>)_param_mis_yaw_err, // yaw-error threshold
-                                    (ParamFloat<params_id::MPC_ACC_HOR>)_param_mpc_acc_hor, // acceleration in flight
+                                    (ParamFloat<params_id::MIS_YAW_ERR>)_param_mis_yaw_err,       // yaw-error threshold
+                                    (ParamFloat<params_id::MPC_ACC_HOR>)_param_mpc_acc_hor,       // acceleration in flight
                                     (ParamFloat<params_id::MPC_ACC_UP_MAX>)_param_mpc_acc_up_max,
                                     (ParamFloat<params_id::MPC_ACC_DOWN_MAX>)_param_mpc_acc_down_max,
                                     (ParamFloat<params_id::MPC_JERK_AUTO>)_param_mpc_jerk_auto,
@@ -147,17 +149,13 @@ protected:
                                     (ParamFloat<params_id::MPC_LAND_CRWL>)_param_mpc_land_crawl_speed,
                                     (ParamInt<params_id::MPC_LAND_RC_HELP>)_param_mpc_land_rc_help,
                                     (ParamFloat<params_id::MPC_LAND_RADIUS>)_param_mpc_land_radius,
-                                    (ParamFloat<params_id::MPC_LAND_ALT1>)
-                                        _param_mpc_land_alt1, // altitude at which we start ramping down speed
-                                    (ParamFloat<params_id::MPC_LAND_ALT2>)
-                                        _param_mpc_land_alt2, // altitude at which we descend at land speed
-                                    (ParamFloat<params_id::MPC_LAND_ALT3>)
-                                        _param_mpc_land_alt3, // altitude where we switch to crawl speed, if LIDAR available
+                                    (ParamFloat<params_id::MPC_LAND_ALT1>)_param_mpc_land_alt1, // altitude at which we start ramping down speed
+                                    (ParamFloat<params_id::MPC_LAND_ALT2>)_param_mpc_land_alt2, // altitude at which we descend at land speed
+                                    (ParamFloat<params_id::MPC_LAND_ALT3>)_param_mpc_land_alt3, // altitude where we switch to crawl speed, if LIDAR available
                                     (ParamFloat<params_id::MPC_Z_V_AUTO_UP>)_param_mpc_z_v_auto_up,
                                     (ParamFloat<params_id::MPC_Z_V_AUTO_DN>)_param_mpc_z_v_auto_dn,
                                     (ParamFloat<params_id::MPC_TKO_SPEED>)_param_mpc_tko_speed,
-                                    (ParamFloat<params_id::MPC_TKO_RAMP_T>)
-                                        _param_mpc_tko_ramp_t // time constant for smooth takeoff ramp
+                                    (ParamFloat<params_id::MPC_TKO_RAMP_T>)_param_mpc_tko_ramp_t // time constant for smooth takeoff ramp
     );
 
 private:
@@ -166,21 +164,18 @@ private:
 
     uORB::SubscriptionData<position_setpoint_triplet_s> _sub_triplet_setpoint{ORB_ID(position_setpoint_triplet)};
 
-    matrix::Vector3f
-        _triplet_target; /**< current triplet from navigator which may differ from the intenal one (_target) depending on the vehicle state. */
-    matrix::Vector3f
-        _triplet_prev_wp; /**< previous triplet from navigator which may differ from the intenal one (_prev_wp) depending on the vehicle state.*/
-    matrix::Vector3f
-                     _triplet_next_wp; /**< next triplet from navigator which may differ from the intenal one (_next_wp) depending on the vehicle state.*/
-    matrix::Vector3f _closest_pt;      /**< closest point to the vehicle position on the line previous - target */
+    matrix::Vector3f _triplet_target;                /**< current triplet from navigator which may differ from the intenal one (_target) depending on the vehicle state. */
+    matrix::Vector3f _triplet_prev_wp;               /**< previous triplet from navigator which may differ from the intenal one (_prev_wp) depending on the vehicle state.*/
+    matrix::Vector3f _triplet_next_wp;               /**< next triplet from navigator which may differ from the intenal one (_next_wp) depending on the vehicle state.*/
+    matrix::Vector3f _closest_pt;                    /**< closest point to the vehicle position on the line previous - target */
 
     hrt_abstime _time_last_cruise_speed_override{0}; ///< timestamp the cruise speed was last time overridden using DO_CHANGE_SPEED
 
-    MapProjection _reference_position{};    /**< Class used to project lat/lon setpoint into local frame. */
-    float         _reference_altitude{NAN}; /**< Altitude relative to ground. */
-    hrt_abstime   _time_stamp_reference{0}; /**< time stamp when last reference update occured. */
+    MapProjection _reference_position{};             /**< Class used to project lat/lon setpoint into local frame. */
+    float         _reference_altitude{NAN};          /**< Altitude relative to ground. */
+    hrt_abstime   _time_stamp_reference{0};          /**< time stamp when last reference update occured. */
 
-    WeatherVane _weathervane{this}; /**< weathervane library, used to implement a yaw control law that turns the vehicle nose into the wind */
+    WeatherVane _weathervane{this};                  /**< weathervane library, used to implement a yaw control law that turns the vehicle nose into the wind */
 
     matrix::Vector3f _initial_land_position;
 
