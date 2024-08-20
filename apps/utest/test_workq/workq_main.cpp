170365b757bb7c676c48e99879f6b4c96da1b0f2
无烬 <xueyuankui.good@163.com>
2024-08-21 00:07:09 +0800

    fmu-v5测试：测试一个workq的执行

--- apps/utest/test_workq/workq_main.cpp
+++ apps/utest/test_workq/workq_main.cpp
@@ -6,10 +6,16 @@ using namespace nextpilot;
 
 static wq_config_t sim_rate_ctrl{"wq:sim_rate_ctrl", 4096 * 10, 0};
 
-class SimRateControl : public ModuleCommand<SimRateControl>, public nextpilot::WorkItemScheduled {
+/**
+ * @brief
+ *
+ * ModuleCommand<SimRateControl, 6>: 最多创建6个实例
+ */
+class SimRateControl : public ModuleCommand<SimRateControl, 6>, public nextpilot::WorkItemScheduled {
 public:
-    SimRateControl() :
+    SimRateControl(int idx) :
         WorkItemScheduled(MODULE_NAME, sim_rate_ctrl) {
+        _idx = idx;
     }
 
     ~SimRateControl() {
@@ -17,7 +23,8 @@ public:
 
     /** @see ModuleCommand */
     static SimRateControl *instantiate(int argc, char *argv[]) {
-        SimRateControl *instance = new SimRateControl();
+        int             idx      = atoi(argv[1]);
+        SimRateControl *instance = new SimRateControl(idx);
         return instance;
     }
 
@@ -41,15 +48,35 @@ private:
         static uint32_t cnt = 0;
         if (should_exit()) {
         }
-        rt_thread_mdelay(1000);
-        rt_kprintf("======%ld, %d\n", rt_tick_get(), cnt++);
+        // rt_thread_mdelay(1000);
+        rt_tick_t start = rt_tick_get();
+        while (rt_tick_get() - start <= 2) {
+        }
+        // static double total = 0.0f;
+        // // 执行一些很耗时的操作
+        // for (uint32_t sleep = 0; sleep < 500000; ++sleep) {
+        //     total += 1.0 / sleep;
+        // }
+        // if (_idx == 0) {
+        //     rt_kprintf("======idx=%d, %ld, %d\n", _idx, rt_tick_get(), cnt++);
+        // }
     }
+
+    int _idx;
 };
 
 int sim_rate_ctrl_start() {
-    const char *argv[] = {"sim_rate_control", "start"};
+    int         ret;
+    const char *argv[] = {"sim_rate_control", "start", "0"};
     int         argc   = sizeof(argv) / sizeof(argv[0]);
-    return SimRateControl::main(argc, (char **)argv);
+    ret                = SimRateControl::main(argc, (char **)argv);
+
+
+    // const char *argv1[] = {"sim_rate_control1", "start", "1"};
+    // int         argc1   = sizeof(argv1) / sizeof(argv1[0]);
+    // ret                 = SimRateControl::main(argc1, (char **)argv1);
+
+    return ret;
 }
 
 INIT_APP_EXPORT(sim_rate_ctrl_start);
